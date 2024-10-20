5b149d389b76f7d6d03fd3a595f1f2bd3051f05f
ninja-con-gafas <absdarekar01@gmail.com>
2024-10-20 10:13:41 +0530

    feat: Interact with Google Gemini through API docs: Update README.md  to include a section on Google Gemini
    - Implemented `get_response` to get response for a prompt sent to Gemini 1.5 Flash LLM.

---
+++ ai.py
@@ -0,0 +1,43 @@
+"""
+The module provides utilities to interact with Google AI.
+"""
+
+from requests import post, Response
+
+def get_response(api_key: str, prompt:str) -> Response:
+    """
+    Sends a POST request to the Google Gemini Language Model API to generate content based on the provided prompt.
+
+    args:
+        api_key (str): The API key used for authentication with the Google Gemini Language Model API.
+        prompt (str): The text prompt to send to the API for content generation.
+
+    returns:
+        Response: The response object from the API call containing the generated content and response details.
+    """
+    headers = {
+        'Content-Type': 'application/json',
+    }
+
+    params = {
+        'key': api_key,
+    }
+
+    json_data = {
+        'contents': [
+            {
+                'parts': [
+                    {
+                        'text': prompt,
+                    },
+                ],
+            },
+        ],
+    }
+
+    return post(
+        url='https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent',
+        params=params,
+        headers=headers,
+        json=json_data,
+    )
