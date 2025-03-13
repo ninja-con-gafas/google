018e531c6f1047a94c7b31dd4dbe76c50623b993
Atharv Darekar <absdarekar01@gmail.com>
2025-03-13 16:39:46 +0530

    feat: Convert all worksheets in a Google Sheet spreadsheet into pandas DataFrames  docs: Update README.md to include a section on Google Sheets
    - Implemented `get_worksheets` function using Google Sheets API and a service account.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Repository Overview
 
 The repository features a collection of modules to interact with Google products and services.
+
+**1. Google Sheets**:  
+  Facilitates interactions with Google Sheets through the Google Sheets API and third party libraries.
---
+++ sheets.py
@@ -0,0 +1,32 @@
+"""
+The module provides utilities to interact with Google Sheets using a service account.
+"""
+
+from google.oauth2.service_account import Credentials
+from gspread import authorize, Spreadsheet
+from pandas import DataFrame
+from typing import Dict, List
+
+def get_worksheets(spreadsheet_name: str, credentials: Credentials) -> Dict[str, DataFrame]:
+
+    """
+    Retrieves all worksheets from a specified spreadsheet and converts each worksheet into a DataFrame.
+    The DataFrame is created using the first row as column headers and the remaining rows as data. The spreadsheet must
+    be shared with the service account using its client email.
+
+    args:
+        spreadsheet_name (str): The name of the spreadsheet to be accessed.
+
+    returns:
+        Dict[str, DataFrame]: A dictionary where the keys are worksheet titles and the values are DataFrames
+                              representing the content of each worksheet.
+
+    raises:
+        None
+    """
+
+    spreadsheet: Spreadsheet = authorize(credentials).open(spreadsheet_name)
+
+    return {worksheet.title: DataFrame(data=worksheet.get_all_values()[1:],
+                                       columns=worksheet.get_all_values()[0])
+            for worksheet in spreadsheet.worksheets()}
