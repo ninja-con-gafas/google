0863dc9f52a2f084172727ef99b0efc243781774
Atharv Darekar <absdarekar01@gmail.com>
2025-03-13 17:13:00 +0530

    feat: Create Google API client resource for a services account
    - Added `build_resource_for_service_account` function to create a resource object using service account credentials.

--- iam.py
+++ iam.py
@@ -2,6 +2,34 @@
 The module handles identity and access management for Google services.
 """
 
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build, Resource
+from typing import List
+
+def build_resource_for_service_account(path: str, scopes: List[str], service_name: str, version: str) -> Resource:
+
+    """
+    Builds and returns a Google API client Resource object for a specified service using service account credentials
+    from a JSON key file.
+
+    args:
+        path (str): The file path to the service account key JSON file.
+        scopes (List[str]): A list of OAuth 2.0 scopes to authorize for the credentials.
+        service_name (str): The name of the Google API service.
+        version (str): The version of the API to use.
+
+    returns:
+        Resource: A Resource object that can be used to interact with the specified Google API service.
+
+    raises:
+        google.auth.exceptions.GoogleAuthError: If loading the credentials or building the resource fails.
+    """
+
+    print(f"Loading credentials for Google Service Account from the file: {path}")
+    return (build(credentials=Credentials.from_service_account_file(filename=path, scopes=scopes),
+                  serviceName=service_name,
+                  version=version))
+
 def read_api_key(path: str) -> str:
 
     """
